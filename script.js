f7a2e585a8c4c21f954be8466da4561eb2fd9768
Dea Septina <dea.septina08@gmail.com>
2025-07-02 15:26:48 +0700

    Update website

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
   toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
   });
+
+ 
+function kirimWhatsApp() {
+    var nama = document.getElementById("nama").value;
+    var email = document.getElementById("email").value;
+    var telepon = document.getElementById("telepon").value;
+    var gender = document.getElementById("gender").value;
+    var tanggalLahir = document.getElementById("tanggal").value;
+    var poli = document.getElementById("poli").value;
+    var tanggalKunjungan = document.getElementById("tanggal_kunjungan").value;
+    var keluhan = document.getElementById("keluhan").value;
+
+    var pesan = `Halo Admin, saya ingin daftar:\n\nNama: ${nama}\nEmail: ${email}\nTelepon: ${telepon}\nJenis Kelamin: ${gender}\nTanggal Lahir: ${tanggalLahir}\nPoli: ${poli}\nTanggal Kunjungan: ${tanggalKunjungan}\nKeluhan: ${keluhan}`;
+
+    // Nomor WhatsApp tujuan (ganti dengan nomor admin)
+    var nomorTujuan = "6285887195694"; // format: 62 + nomor tanpa 0
+
+    var url = `https://wa.me/${nomorTujuan}?text=${encodeURIComponent(pesan)}`;
+
+    window.open(url, '_blank');
+}
